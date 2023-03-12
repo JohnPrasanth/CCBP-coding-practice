ea96d32473d3f9d60c6b697912cf79f0a7a9e499
john <johnrockz429@gmail.com>
2023-03-12 09:26:44 +0000

    added check

---
+++ check.js
@@ -0,0 +1,2 @@
+const ck = require("./index.js");
+console.log(ck(5));
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var addDays = require("date-fns/addDays");
-let date = new Date("2020-08-22");
+const date = new Date("2020-08-22");
 function adddays(days) {
   return addDays(date, days);
 }
